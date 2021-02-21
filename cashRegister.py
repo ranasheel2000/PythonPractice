f4c7cf6631dff47ae8c719b379a2e88b8555b847
Sheel Rana <ranasheel2000@gmail.com>
2021-02-22 01:12:16 +0900

    Update cashRegister.py

--- cashRegister.py
+++ cashRegister.py
@@ -1,96 +1,96 @@
-bills = {.01:'PENNY',
-        .05:'NICKEL',
-        0.10:'DIME',
-        0.25:'QUARTER',
-        0.50:'HALF DOLLAR',
-        1.00:'ONE',
-        2.00:'TWO',
-        5.00:'FIVE',
-        10.0:'TEN',
-        20.00:'TWENTY',
-        50.00:'FIFTY',
-        100.00:'ONE HUNDRED'}
+register={.01: 'PENNY',
+.05:'NICKEL',
+.10:'DIME',
+.25:'QUARTER',
+.50:'HALF DOLLAR',
+1.00:'ONE',
+2.00:'TWO',
+5.00:'FIVE',
+10.00:'TEN',
+20.00:'TWENTY',
+50.00:'FIFTY',
+100.00:'ONE HUNDRED'}
 
- 
-
-changes1=[]
-def changes(change_value):
-    #print(change_value)
-    #changes1=[]
-    if(change_value>=100.00):
-        changes1.append(bills[100.00])
-        value=float("{0:.2f}".format(change_value-100.00))
-        return changes(value)
-    elif(change_value>=50.00):
-        changes1.append(bills[50.00])
-        value=float("{0:.2f}".format(change_value-50.00))
-        return changes(value)
-    if(change_value>=20.00):
-        changes1.append(bills[20.00])
-        value=float("{0:.2f}".format(change_value-20.00))
-        return changes(value)
-    elif(change_value>=10.00):
-        changes1.append(bills[10.00])
-        value=float("{0:.2f}".format(change_value-10.00))
-        return changes(value)
-    elif(change_value>=5.00):
-        changes1.append(bills[5.00])
-        value=float("{0:.2f}".format(change_value-5.00))
-        return changes(value)
-    if(change_value>=2.00):
-        changes1.append(bills[2.00])
-        value=float("{0:.2f}".format(change_value-2.00))
-        return changes(value)
-    elif(change_value>=1.00):
-        changes1.append(bills[1.00])
-        value=float("{0:.2f}".format(change_value-1.00))
-        return changes(value)
-    elif(change_value>=0.50):
-        changes1.append(bills[0.50])
-        value=float("{0:.2f}".format(change_value-0.50))
-        return changes(value)
-    if(change_value>=0.25):
-        changes1.append(bills[0.25])
-        value=float("{0:.2f}".format(change_value-0.25))
-        return changes(value)
-    elif(change_value>=0.10):
-        changes1.append(bills[0.10])
-        value=float("{0:.2f}".format(change_value-0.10))
-        return changes(value)
-    elif(change_value>=0.05):
-        changes1.append(bills[0.05])
-        value=float("{0:.2f}".format(change_value-0.05))
-        return changes(value)
-    elif(change_value>=0.01):
-        changes1.append(bills[0.01])
-    return changes1
-
- 
-
-def cash_register(pp,ch):
-    change_value=float("{0:.2f}".format(ch-pp))
-    changes(change_value)
-    return(change_value)
-
- 
-
-while(True):
-    print("Please enter PP and CH values separated by semicolon")
-    print("Please enter 0;0 to exit")
-    customer_input =input()
-    inputs=customer_input.split(";")
-    #if(len(inputs)!=2 or input[1] is ""):
-    #    print("wrong input")
-    pp = float(inputs[0])
-    ch = float(inputs[1])
-    #print(pp)
-    #print(ch)
-    if(ch==pp==0):
-        exit()
-    if(ch<pp):
-        print("ERROR")
-    elif(ch==pp):
-        print("ZERO")
-    else:
-
-        print(cash_register(pp,ch))
+def calculate(pp,ch):
+    #left_cash=float("{0:.2f}".format(ch-pp))
+    left_cash=float(ch)-float(pp)
+    print(left_cash)
+    result=[]
+    while(left_cash>0.01):
+        if(left_cash>=100.00):
+            result.append(register[100.00])
+            left_cash=float("{0:.2f}".format(left_cash-100.00))
+            print(result)
+            print(left_cash)
+        elif(left_cash>=50.00):
+            result.append(register[50.00])
+            left_cash=float("{0:.2f}".format(left_cash-50.00))
+            print(result)
+            print(left_cash)
+        elif(left_cash>=20.00):
+            result.append(register[20.00])
+            left_cash=float("{0:.2f}".format(left_cash-20.00))
+            print(result)
+            print(left_cash)
+        elif(left_cash>=10.00):
+            result.append(register[10.00])
+            left_cash=float("{0:.2f}".format(left_cash-10.00))
+            print(result)
+            print(left_cash)
+        elif(left_cash>=5.00):
+            result.append(register[5.00])
+            left_cash=float("{0:.2f}".format(left_cash-5.00))
+            print(result)
+            print(left_cash)
+        elif(left_cash>=2.00):
+            result.append(register[2.00])
+            left_cash=float("{0:.2f}".format(left_cash-2.00))
+            print(result)
+            print(left_cash)
+        elif(left_cash>=1.00):
+            result.append(register[1.00])
+            left_cash=float("{0:.2f}".format(left_cash-1.00))
+            print(result)
+            print(left_cash)
+        elif(left_cash>=0.50):
+            result.append(register[0.50])
+            left_cash=float("{0:.2f}".format(left_cash-0.50))
+            print(result)
+            print(left_cash)
+        elif(left_cash>=0.25):
+            result.append(register[0.25])
+            left_cash=float("{0:.2f}".format(left_cash-0.25))
+            print(result)
+            print(left_cash)
+        elif(left_cash>=0.10):
+            result.append(register[0.10])
+            left_cash=float("{0:.2f}".format(left_cash-0.10))
+            print(result)
+            print(left_cash)
+        elif(left_cash>=0.5):
+            result.append(register[0.5])
+            left_cash=float("{0:.2f}".format(left_cash-0.5))
+            print(result)
+            print(left_cash)
+        elif(left_cash>=0.1):
+            result.append(register[0.1])
+            left_cash=float("{0:.2f}".format(left_cash-0.1))
+            print(result)
+            print(left_cash)
+    return(sorted(result))
+#import pdb
+#pdb.set_trace()
+user_input=input()
+seperated_input=user_input.split(';')
+output=""
+pp=seperated_input[0]
+ch=seperated_input[1]
+if(ch<pp):
+    print("ERROR")
+elif(ch==pp):
+    print("ZERO")
+else:
+    result=calculate(pp,ch)
+    for x in result:
+        output=','.join(x)
+    print(output)
